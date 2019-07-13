43d5aa304f1db04f17480b9e9a919bd63e5e043e
Wesley Ferreira <wsf.ley@gmail.com>
2019-07-13 17:59:39 -0300

    post implement

--- index.php
+++ index.php
@@ -9,14 +9,31 @@
     <title>Prova 2 Bimestre</title>
 </head>
 <?php
+$arq = fopen("data/violencia-domestica-df.csv", "r");
+$d = fgetcsv($arq);
 $get_ano = $_GET['ano'];
+$post_localidade = $_POST['localidade'];
+
 function print_td($data_csv, $ano)
 {
-    $indice = $ano - 2010;
-    echo ($indice);
+    if ($ano) {
+        $indice = $ano - 2010;
+        echo "<tr>";
+        echo "<td> $data_csv[0]</td>";
+        echo "<td> $data_csv[$indice]</td>";
+        echo "</tr>";
+    }
+}
+function print_td_post($data_csv)
+{
     echo "<tr>";
     echo "<td> $data_csv[0]</td>";
-    echo "<td> $data_csv[$indice]</td>";
+    echo "<td> $data_csv[1]</td>";
+    echo "<td> $data_csv[2]</td>";
+    echo "<td> $data_csv[3]</td>";
+    echo "<td> $data_csv[4]</td>";
+    echo "<td> $data_csv[5]</td>";
+    echo "<td> $data_csv[6]</td>";
     echo "</tr>";
 }
 function soma_casos($data_csv, $ano)
@@ -27,27 +44,56 @@ function soma_casos($data_csv, $ano)
 function print_th($data_csv, $ano)
 {
     $indice = $ano - 2010;
-    echo ($indice);
     echo "<thead>";
     echo "<th> $data_csv[0]</th>";
     echo "<th> $data_csv[$indice]</th>";
     echo "</thead>";
 }
+function print_th_post($data_csv)
+{
+    echo "<thead>";
+    echo "<th> $data_csv[0]</th>";
+    echo "<th> $data_csv[1]</th>";
+    echo "<th> $data_csv[2]</th>";
+    echo "<th> $data_csv[3]</th>";
+    echo "<th> $data_csv[4]</th>";
+    echo "<th> $data_csv[5]</th>";
+    echo "<th> $data_csv[6]</th>";
+    echo "</thead>";
+}
+
+function search_localidade($busca, $data, $arq)
+{
+    $indexI = 0;
+    while ($data) {
+        if ($indexI != 0) {
+            if ($busca == $data[0]) {
+                
+                return $data;
+            }
+        }
+        $indexI += 1;
+        $data = fgetcsv($arq);
+    }
+}
 
 
-$arq = fopen("data/violencia-domestica-df.csv", "r");
-$d = fgetcsv($arq);
 ?>
 
 <body>
+    <div class="inputs">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            <input type="text" name="localidade">
+            <input type="submit" value="buscar">
+        </form>
+    </div>
+
     <div id="outputs">
         <table id="output">
-            <?php
-            print_th($d, $get_ano);
-
+            <?php            
             $data = $d;
-            
             if ($get_ano) {
+                print_th($d, $get_ano);
                 $indexI = 0;
                 while ($data) {
                     if ($indexI != 0) {
@@ -58,9 +104,21 @@ $d = fgetcsv($arq);
                     $indexI += 1;
                 }
             }
+            if ($post_localidade) {
+                $data_find = search_localidade($post_localidade, $data, $arq);
+                for ($i=1; $i < count($data_find) ; $i++) { 
+                    $valor += $data_find[$i];
+                                }
+                print_th_post($data);
+                print_td_post($data_find);
+            }
+
             fclose($arq);
             ?>
         </table>
+    </div>
+    <div>
+        <h3>Somatorio dos casos: <?php echo $valor; ?></h1>
     </div>
 </body>
 
