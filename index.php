90d9cd65f1ae3881f0c5d1d4a5c0298bdf546d56
Wesley Ferreira <wsf.ley@gmail.com>
2019-07-13 16:09:12 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prova 2 Bimestre</title>
+</head>
+
+
+
+    
+<body>
+    <div id="outputs">
+        <table id="output">
+            <thead>
+                <td>Violencia Domestica</td>
+            </thead>
+            <?php
+                
+
+            ?>
+                
+
+
+        </table>
+    </div>
+</body>
+
+</html>
